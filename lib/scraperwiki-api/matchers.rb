19f4094490fb9857cf6f7b61a786be42911b787a
James McKinney <james@slashpoundbang.com>
2012-05-27 15:11:43 -0400

    add :be_broken matcher

--- lib/scraperwiki-api/matchers.rb
+++ lib/scraperwiki-api/matchers.rb
@@ -13,12 +13,20 @@ module ScraperWiki
           @info = info
         end
 
-        def failure_message
+        def failure_phrase
           @expected
         end
 
+        def negative_failure_phrase
+          failure_phrase
+        end
+
         def failure_message_for_should
-          [failure_message, ScraperWiki::API.scraper_url(@info['short_name'])].join "\n"
+          [failure_phrase, ScraperWiki::API.scraper_url(@info['short_name'])].join "\n"
+        end
+
+        def failure_message_for_should_not
+          failure_message_for_should
         end
       end
 
@@ -28,7 +36,7 @@ module ScraperWiki
           info['privacy_status'] == @expected
         end
 
-        def failure_message
+        def failure_phrase
           "expected #{@info['short_name']} to be #{@expected}"
         end
       end
@@ -50,7 +58,7 @@ module ScraperWiki
           end
         end
 
-        def failure_message
+        def failure_phrase
           "expected #{@info['short_name']} to be editable by #{@expected}"
         end
       end
@@ -64,7 +72,7 @@ module ScraperWiki
           info['run_interval'] == ScraperWiki::API::RUN_INTERVALS[@expected]
         end
 
-        def failure_message
+        def failure_phrase
           if @expected == -1
             "expected #{@info['short_name']} to never run"
           else
@@ -96,7 +104,7 @@ module ScraperWiki
           raise NotImplementerError
         end
 
-        def failure_message
+        def failure_phrase
           "#{@info['short_name']} #{failure_predicate}: #{difference.join ', '}"
         end
       end
@@ -133,12 +141,36 @@ module ScraperWiki
           info['datasummary']['tables'][@table]['count'] == @expected
         end
 
-        def failure_message
+        def failure_phrase
           "expected #{@info['short_name']} to have #{@expected} rows, not #{@info['datasummary']['tables'][@table]['count']}"
         end
       end
       def have_total_rows_of(expected)
         CountMatcher.new expected
+      end
+
+      class RunEventsMatcher < CustomMatcher
+        def last_run
+          @info['runevents'][0]
+        end
+      end
+
+      class ExceptionMessageMatcher < RunEventsMatcher
+        def matches?(info)
+          super
+          exception_message
+        end
+
+        def exception_message
+          last_run['exception_message']
+        end
+
+        def failure_phrase
+          "#{@info['short_name']} is broken: #{exception_message}"
+        end
+      end
+      def be_broken
+        ExceptionMessageMatcher.new nil
       end
     end
   end
